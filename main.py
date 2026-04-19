243fb1f52e327c295217729bedc3bf793eb45fb2
deepakkumarmcs18 <163831846+deepakkumarmcs18@users.noreply.github.com>
2026-04-20 02:11:18 +0530

    template updated

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
-
+app.mount("/static", StaticFiles(directory="static"), name="static")
 posts = {
     "id":101,
     "name": "Deepak kumar",
     "title": "Software developer",
     "address": "Jharkhand"
+}
 
-
-@app.get("/")
+@app.get("/home", include_in_schema=False, name="home")
 def home(request: Request):
     return templates.TemplateResponse(request, "home.html", {"posts": posts})
